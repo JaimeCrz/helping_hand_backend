680e551d6af6e5dc77e5aea340fb66d07c47eee9
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-25 11:22:12 +0200

    fixing last bugs

--- spec/requests/api/v1/requester/finalized_task_spec.rb
+++ spec/requests/api/v1/requester/finalized_task_spec.rb
@@ -9,11 +9,10 @@ RSpec.describe 'PUT api/v1/tasks/:id', type: :request do
   let(:provider_credentials) { provider.create_new_auth_token }
   let(:provider_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(provider_credentials) }
 
-
-  let(:task) { create(:task, status: 'delivered', provider: provider, user: user) }
-  let!(:task_items) { 5.times { create(:task_item, task: task) } }
-
   describe 'Succesfully finalizes the task' do
+    let(:task) { create(:task, status: 'delivered', provider: provider, user: user) }
+    let!(:task_items) { 5.times { create(:task_item, task: task) } }
+  
     before do
       put "/api/v1/tasks/#{task.id}",
       params: { activity: 'finalized' },
@@ -30,7 +29,33 @@ RSpec.describe 'PUT api/v1/tasks/:id', type: :request do
   end
 
   describe 'Unsuccesfully finalizes the task' do
+    let(:another1_user) { create(:user) }
+    let(:another1_user_credentials) { user.create_new_auth_token }
+    let(:another1_user_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
+  
+    let(:task) { create(:task, status: 'delivered', provider: provider, user: user) }
+    let!(:task_items) { 5.times { create(:task_item, task: task) } }
+
+    let(:another_task) { create(:task, status: 'claimed', provider: provider, user: another1_user) }
+    let!(:another_task_items) { 5.times { create(:task_item, task: another_task) } }
+
     describe 'provider tries to finalize the otder' do
+      before do
+        put "/api/v1/tasks/#{another_task.id}",
+        params: { activity: 'finalized' },
+        headers: another1_user_headers
+      end
+
+      it 'returns a 500 response status' do
+        expect(response).to have_http_status 500
+      end
+
+      it 'response with an error message' do
+        expect(response_json['error_message']).to eq 'We are experiencing internal errors. Please refresh the page and contact support. No activity specified'
+      end
+    end
+  
+    describe 'provider tries to finalize the order' do
       before do
         put "/api/v1/tasks/#{task.id}",
         params: { activity: 'finalized' },
